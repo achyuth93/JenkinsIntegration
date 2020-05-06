34cb1f0a142d8e33fa22a1f8369eb73af3604144
Achyuth <achyuth12693@gmail.com>
2020-05-06 16:24:28 +0530

    hello

--- src/main/java/testJenkins/JenkinsDemo.java
+++ src/main/java/testJenkins/JenkinsDemo.java
@@ -3,7 +3,7 @@ package testJenkins;
 public class JenkinsDemo {
 
 	public static void main(String[] args) {
-		System.out.println("This is a Jenkins Demo Project with github .");
+		System.out.println("This is a Jenkin Project with github .");
 
 	}
 
